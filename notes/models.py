2886d97e050df987f5742d9bd37c04644bc11644
Shubham Nishad <shubhamnishad25@gmail.com>
2017-07-04 12:19:04 +0530

    changed the organization of the project
    I changed the organization so that it was easy to troubleshoot while
    hosting on pythonanywhere.com

---
+++ notes/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+ 
+class Notes(models.Model):
+	label = models.CharField(max_length=200)
+	body = models.TextField(max_length=20000)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	tags = models.ManyToManyField('Tag', related_name='notes', blank=True)
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="notes_owner",blank=True,null=True)
+	def __str__(self):
+		return self.label
+		
+
+
+
+class Tag(models.Model):
+	label = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200)
+	def __str__(self):
+		return self.label
+
+
+
+
+
+
+
---
+++ toki/urls.py
@@ -0,0 +1,32 @@
+"""toki URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+import profiles.urls
+import accounts.urls
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^about/$', views.AboutPage.as_view(), name='about'),
+    url(r'^users/', include(profiles.urls, namespace='profiles')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(accounts.urls, namespace='accounts')),
+    url(r'^notes/', include('notes.urls', namespace='notes')),
+]
